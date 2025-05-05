2542b395e710d654bb61372b5d711147e2429afc
danielwolber-wood <daniel@wolber-wood.com>
2025-05-05 12:47:17 -0500

    1.5 Completed

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
+	"io"
 	"net/http"
+	"time"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +31,47 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
-
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	const maxMemory = 10 << 20
+	if err := r.ParseMultipartForm(maxMemory); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Could not parse multipart form", err)
+		return
+	}
+	file, fileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Could not read thumbnail file", err)
+		return
+	}
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", fileHeader)
+	fmt.Printf("File Size: %+v\n", fileHeader.Size)
+	fmt.Printf("MIME Header: %+v\n", fileHeader.Header)
+	var imageData []byte
+	imageData, err = io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Could not read thumbnail file", err)
+	}
+	data, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not get video", err)
+	}
+	if data.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You are not authorized to upload this thumbnail", err)
+	}
+	tb := thumbnail{data: imageData, mediaType: fileHeader.Header.Get("Content-Type")}
+	//respondWithJSON(w, http.StatusOK, struct{}{})
+	videoThumbnails[videoID] = tb
+	thumbnailURL := "http://localhost:8091/api/thumbnails/" + videoID.String()
+	video := database.Video{
+		ID:                data.ID,
+		CreatedAt:         data.CreatedAt,
+		UpdatedAt:         time.Now(),
+		ThumbnailURL:      &thumbnailURL,
+		VideoURL:          data.VideoURL,
+		CreateVideoParams: data.CreateVideoParams,
+	}
+	cfg.db.UpdateVideo(video)
+	respondWithJSON(w, 200, video)
 }
